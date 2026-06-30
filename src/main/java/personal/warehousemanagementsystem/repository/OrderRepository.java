9b9aaddb7733713feb5afc3ad8d160ca7ebea692
David Jovanovski <dasejovanovski@live.com>
2026-06-30 15:27:21 +0200

    Fixed status field place

--- src/main/java/personal/warehousemanagementsystem/repository/OrderRepository.java
+++ src/main/java/personal/warehousemanagementsystem/repository/OrderRepository.java
@@ -3,13 +3,18 @@ package personal.warehousemanagementsystem.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import personal.warehousemanagementsystem.models.Order;
+import personal.warehousemanagementsystem.models.enums.Status;
 
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Optional<Order> findByInvoiceNumber(int invoiceNumber);
+
     Optional<Order> findByCreatedAtBetween(LocalDate start, LocalDate end);
+
+    List<Order> findByStatus(Status status);
 }
