f45256ad1f5f78534ca922f6109375be9362d518
David Jovanovski <dasejovanovski@live.com>
2026-07-20 22:07:49 +0200

    Added OrderNotFoundException to handle nonexistent orders more concisely

--- src/main/java/personal/warehousemanagementsystem/models/exceptions/OrderNotFoundException.java
+++ src/main/java/personal/warehousemanagementsystem/models/exceptions/OrderNotFoundException.java
@@ -1,5 +1,9 @@
 package personal.warehousemanagementsystem.models.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class OrderNotFoundException extends RuntimeException {
     public OrderNotFoundException(Long id) {
         super(String.format("Order with id: %d doesn't exist", id));
