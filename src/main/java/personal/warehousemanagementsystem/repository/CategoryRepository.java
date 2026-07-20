fd909632a32705c076bb4db402479e0ce267e7e5
David Jovanovski <dasejovanovski@live.com>
2026-07-20 22:06:52 +0200

    Edited CategoryRepository to match impl better

--- src/main/java/personal/warehousemanagementsystem/repository/CategoryRepository.java
+++ src/main/java/personal/warehousemanagementsystem/repository/CategoryRepository.java
@@ -10,6 +10,4 @@ import java.util.Optional;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Optional<Category> findByName(String name);
-
-    Long id(Long id);
 }
